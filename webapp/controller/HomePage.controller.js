9141d1867b46c95c1d04355516b3b9c8cdf0d12c
Aswinraj DC <aswinraj.dc@sap.com>
2024-03-29 05:34:30 +0000

    Fragment and i18n

--- webapp/controller/HomePage.controller.js
+++ webapp/controller/HomePage.controller.js
@@ -2,12 +2,13 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter"
+    "sap/ui/model/Filter",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox, JSONModel,Filter) {
+    function (Controller, MessageBox, JSONModel,Filter, Fragment) {
         "use strict";
 
         return Controller.extend("com.sap.project1.controller.HomePage", {
@@ -20,6 +21,8 @@ sap.ui.define([
                 console.log(oModel.getData())
 
                 this.getView().setModel(oModel);
+
+                this.loadFragment();
             },
             onButtonClicked: function () {
                 MessageBox.information("Button Clicked");
@@ -42,6 +45,47 @@ sap.ui.define([
 
                oBinding.filter(aFilters)
 
+            },
+
+            onPress: function (oEvent) {
+
+                const sPath = oEvent.getSource().getBindingContext().sPath,
+                    oModel = this.getView().getModel();
+
+                const oSelectedDetail = oModel.getProperty(sPath);
+
+                const aImages = oSelectedDetail.images.map(url => ({ url }));
+
+                oModel.setProperty("/oSelectedItem", aImages);
+
+                this.oDetailsFragment.open();
+
+            },
+
+            loadFragment: function () {
+
+                if (!this.oDetailsFragment) {
+                    Fragment.load({
+                        name: "com.sap.project1.view.fragment.Details",
+                        controller: this
+                    }).then((oDialog) => {
+
+
+                        this.oDetailsFragment = oDialog;
+
+                        this.getView().addDependent(this.oDetailsFragment);
+
+
+                    }).catch((oErr) => {
+                        console.log(oErr);
+                    })
+                }
+            },
+
+            onClose: function() {
+                this.oDetailsFragment.close();
             }
+
+
         });
     });
